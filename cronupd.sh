a69029b695aa48dab90d8b9d619412e286a3488d
Yuuki Galaxy <galaxy001@gmail.com>
2013-01-26 20:45:39 +0800

    modified:   cronupd.sh

--- cronupd.sh
+++ cronupd.sh
@@ -9,22 +9,31 @@ date
 cd /opt/galaxy/git/12306_ticket_helper_iccfish/
 mv 12306_ticket_helper.user.js 12306_ticket_helper.user.js.old
 mv version.js version.js.old
+mv 12306_ticket_helper.crx 12306_ticket_helper_last.crx
 wget -S http://static.liebao.cn/_softupdate/44/version.js -o updatetime.log
 wget -S http://static.liebao.cn/_softdownload/12306_ticket_helper.user.js -a updatetime.log
+wget -S http://static.liebao.cn/_softdownload/12306_ticket_helper.crx -a updatetime.log
 
-#THEVER=`perl -MPOSIX -lane 'if (/\bversion_12306_helper\b/) {@v=split /=/; $s=@v[-1]; $s=~s/[\s";]//g;print "$s @ ",POSIX::strftime( "%Y-%m-%d %R", localtime());exit;}' version.js`
 THEVER=`perl -MPOSIX -MFile::stat -lane 'if (/\bvar\s+version\b/) {@v=split /=/; $s=@v[-1]; $s=~s/[\s";]//g;$b=stat($ARGV)->[9];print "$s @ ",strftime("%Y-%m-%d %T %Z",localtime($b));exit;}' 12306_ticket_helper.user.js`
-#THEVER=`perl -MPOSIX -MFile::stat -lane 'if (/\bversion_12306_helper\b/) {@v=split /=/; $s=@v[-1]; $s=~s/[\s";]//g;$b=stat($ARGV)->[9];print "$s @ ",strftime("%Y-%m-%d %T %Z",localtime($b));exit;}' version.js`
-# 4.4.2 @ 2013-01-25 13:12
+# 4.5.0 @ 2013-01-26 18:15:56 CST
 #echo $THEVER|awk '{print $1}' > now.ver.user
+JUSTVER=`echo $THEVER|awk '{gsub(/\./,"");print $1}'`
 
 cmp 12306_ticket_helper.user.js 12306_ticket_helper.user.js.old
 if [ $? -ne 0 ]; then
+   unzip -oDDd chrome.new 12306_ticket_helper.crx
+   if [ $? -eq 1 ]; then
+      rm -fr chrome
+      mv chrome.new chrome
+      git add chrome
+   else
+      rm -fr chrome.new
+   fi
+   mv 12306_ticket_helper.crx bak/12306_ticket_helper_${JUSTVER}.crx
+   git add bak/12306_ticket_helper_${JUSTVER}.crx
    git commit . -m "$THEVER" --author='Galaxy <galaxy001@gmail.com>'
    git push
    echo "Updated: $THEVER"
-   #mv now.ver.user last.ver.user
 else
    echo "Still: $THEVER"
-   #rm now.ver.user
 fi
