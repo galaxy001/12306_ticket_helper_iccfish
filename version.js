b82463069b31631d31d99a6a62051976987cb8f0
Galaxy <galaxy001@gmail.com>
2013-07-09 18:45:04 +0800

    '+window.helperVersion+'><\/iframe>')})) @ 2013-07-09 18:45:03 CST

--- version.js
+++ version.js
@@ -1,9 +1,10 @@
-var version_12306_helper = "5.1.0";
-var version_12306_helper_beta = "5.1.0";
+var version_12306_helper = "5.1.1";
+var version_12306_helper_beta = "5.1.1";
 var version_updater = [
 	"* 修正改签页面助手不能运行的BUG",
-	"* 界面调整，工作流程优化"
+	"* 界面调整，工作流程优化",
+	"* (5.11) 修正点击原始『查询』按钮多点会导致提示过多的BUG"
 ];
 var fishlee12306_msgid=17;
-var fishlee12306_msgver="4.6.2";
-var fishlee12306_msg="警告！由于铁道部的缓存比较要命，故意让你看不到票，因此如果你需要整点抢票，请务必使用『等待整点模式』，否则一直刷新的话会导致你晚很多才能看到票！\n\n除此以外，等待倒计时的过程中，建议不要最小化浏览器或切换到其它标签！\n\n另外就是佩服铁道部的开发人员，他们比我勤快，我都想着应该不会改了来着，难得他们还这么关注……";
+var fishlee12306_msgver="4.0.3";
+var fishlee12306_msg="";
